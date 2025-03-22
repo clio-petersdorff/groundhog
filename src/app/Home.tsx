9f011eca10b835213e037c425ecd9b349ba60e90
Clio von Petersdorff <clio.petersdorff@gmail.com>
2025-03-22 16:30:34 +0000

    Updates to app structure

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,16 +1,8 @@
 import { useState, useEffect } from "react";
-import {
-  Center,
-  Stack,
-  Title,
-  Select,
-  Space,
-  Button,
-  Group,
-} from "@mantine/core";
-import getTubeStations from "../hooks/getTubeStations";
-import { IconArrowRight } from "@tabler/icons-react";
-import { useNavigate } from "react-router-dom";
+import { Center, Stack, Title, Space } from "@mantine/core";
+import getTubeStations from "../actions/getTubeStations";
+import Inputs from "./Inputs";
+import Results from "./Results";
 
 interface StationType {
   name: string;
@@ -22,8 +14,7 @@ interface StationType {
 export default function Home() {
   const [allStations, setAllStations] = useState<StationType[]>([]);
   const [selectedStations, setSelectedStations] = useState<StationType[]>([]); // Changed to store StationType
-
-  const navigate = useNavigate(); // Create navigate function
+  const [view, setView] = useState<string>("inputs");
 
   // Fetch stations on component mount
   useEffect(() => {
@@ -35,65 +26,22 @@ export default function Home() {
     fetchStations();
   }, []);
 
-  // Handle station selection (limit to 5)
-  const handleSelect = (value: string | null, index: number) => {
-    const station = allStations.find((station) => station.name === value); // Find the full station object by name
-    if (station) {
-      setSelectedStations((prevState) => {
-        const newSelectedStations = [...prevState];
-        newSelectedStations[index] = station;
-        return newSelectedStations;
-      });
-    }
-  };
-
-  // Add a new select input
-  const handleAddSelect = () => {
-    if (selectedStations.length < 5) {
-      setSelectedStations([...selectedStations, {} as StationType]); // Add empty object as placeholder
-    }
-  };
-
   return (
     <Center h="100vh">
       <Stack w="30em">
         <Title ta="center">Home page</Title>
         <Space h="3em" />
-
-        {/* Dynamic selects based on the current number of selections */}
-        {selectedStations.map((_, index) => (
-          <Select
-            key={index}
-            label={`Station ${index + 1}`}
-            value={selectedStations[index]?.name || ""}
-            onChange={(value) => handleSelect(value, index)}
-            data={allStations.map((item) => item.name)}
-            placeholder={`Select station ${index + 1}`}
-            searchable
-            disabled={selectedStations.length > 5 && !selectedStations[index]} // Disable select when limit is reached
+        {view === "inputs" && (
+          <Inputs
+            setView={setView}
+            allStations={allStations}
+            selectedStations={selectedStations}
+            setSelectedStations={setSelectedStations}
           />
-        ))}
-
-        {/* Button to add more selects up to 5 */}
-        <Center>
-          <Button w="4em" color="cyan" onClick={handleAddSelect}>
-            +
-          </Button>
-        </Center>
-
-        <Space h="2em" />
-        <Group justify="flex-end">
-          {selectedStations[1] && (
-            <Button
-              onClick={() => navigate("/results")}
-              w="7em"
-              color="cyan"
-              rightSection={<IconArrowRight size={14} />}
-            >
-              Next
-            </Button>
-          )}
-        </Group>
+        )}
+        {view === "results" && (
+          <Results stations={selectedStations} setView={setView} />
+        )}
       </Stack>
     </Center>
   );
--- src/app/Results.tsx
+++ src/app/Results.tsx
@@ -1,5 +1,25 @@
+import { Title } from "@mantine/core";
+import getAllCandidates from "../actions/getAllCandidates";
+import { StationType } from "../types/Types";
 import Loading from "./Loading";
 
-export default function Results() {
-  return <Loading />;
+interface ResultsProps {
+  stations: StationType[];
+  setView: (view: string) => void;
+}
+
+export default function Results({ stations }: ResultsProps) {
+  const candidates = getAllCandidates(stations);
+  console.log(candidates);
+
+  if (!candidates) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <Title>All station options</Title>
+      {/* {candidates.map((item) => item.name)} */}
+    </>
+  );
 }
