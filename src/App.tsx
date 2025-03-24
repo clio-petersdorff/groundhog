391d16e66b01916df100fc1554920ccb0dda1aca
Clio von Petersdorff <clio.petersdorff@gmail.com>
2025-03-24 22:11:54 +0000

    connecting backend

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,19 @@ import "@mantine/core/styles.css";
 
 import { MantineProvider } from "@mantine/core";
 import Home from "./app/Home";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 export default function App() {
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8000/api/users");
+    console.log(response.data.users);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
   return (
     <MantineProvider>
       <Home />
